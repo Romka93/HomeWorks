3c5e5d9bcaa4313b2c20e79c1e65b68c1695c428
Ромка <vengerroma3@gmail.com>
2017-05-07 21:00:56 +0300

    day2

--- day1.js
+++ day1.js
@@ -1,3 +1,37 @@
+console.log(Number('100'));
+console.log(+'100');
+console.log(Number('100a'));
+console.log(+'100a');
+console.log(parseInt('10 0a'));
+console.log(parseInt('a10 0a'));
+console.log(parseFloat('10.02a'));
+console.log(parseInt('10.02a'));
 
-var a = 100;
-var f = {1: 'h', 2: 2};
+console.log(Boolean(100));
+console.log(Boolean(0));
+
+console.log(Boolean('100'));
+console.log(Boolean('fsssfs'));
+console.log(!!'100');
+console.log(!!'');
+console.log(!'fs');
+var a;
+console.log(String(true));
+console.log(String(false));
+console.log(a = '' + true);
+console.log('' + false);
+console.log(a);
+console.log(typeof a);
+
+console.log(String({}));
+
+console.log(Boolean({}));
+console.log(Boolean([23]));
+console.log(!!{});
+console.log(!![]);
+
+console.log(typeof function asd() {})
+
+// typeof 1{}
+console.log(typeof 'number');
+
